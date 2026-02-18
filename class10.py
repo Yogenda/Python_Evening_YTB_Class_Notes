544660ae34ddd33125e094bfb2bd4fbf6b69b3c8
Yogendra <yogendrabire61@gmail.com>
2026-02-18 20:06:47 +0530

    class 10 notes

---
+++ class10.py
@@ -0,0 +1,36 @@
+'''
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,j, end="  ")
+    print()
+
+a=5
+b=6
+print("apple"+"Mango")
+print(a,b,"divya","pawani",60)
+
+print("hello world", end=" , ")
+print("hello pawani", end=" , ")
+print("hello divya", end=" , ")
+print("hello baker")
+'''
+
+# square
+'''
+size = 5
+for i in range(size):
+    for j in range(size):
+        print("*",end=" ")
+    print()
+'''
+# print the right angle triange
+'''
+size = 5
+for i in range(size+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+'''
+rows = 5
+for i in range(rows,0,-1):
+    print("*"*i)
