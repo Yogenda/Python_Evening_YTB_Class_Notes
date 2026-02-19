aa332729ec008e2b0cfb664ef828ddadafa3b7ac
Yogendra <yogendrabire61@gmail.com>
2026-02-19 20:04:40 +0530

    string class 11 with loop homework

---
+++ class11.py
@@ -0,0 +1,39 @@
+'''
+first = "Coding"
+mid = "Yogendra"
+last = "Bire"
+print(first,mid,last)
+result = first +" "+mid+" "+last
+print(result)
+print("===",first+mid+last,"===")
+print(result*3)
+'''
+'''
+text = "Python is easy"
+print("Python"in text)
+print("Python" not in text)
+'''
+'''
+result =""
+for word in ["I","love","Python"]:
+    result += word+" "
+print(result)
+
+words = ["I","love","Python"]
+result = " ".join(words)
+print(result)
+'''
+'''
+text = "python"
+print(text[5])
+print(text[-6])
+'''
+text="programming"
+print(text[0:6])
+print(text[::-1])
+print(text[::2])
+print(text[2:5:2])
+print(text[3::])
+print(text[:4:])
+print(text[:4:2])
+
